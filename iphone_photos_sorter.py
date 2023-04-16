f1fe35bcaf81c3ffc76ff0cb3fabb5c12a5ca57f
Jonas Osmann <43489273+jono3030@users.noreply.github.com>
2023-04-16 01:23:53 -0400

    [fix] refactor original image sorter

--- iphone_photos_sorter.py
+++ iphone_photos_sorter.py
@@ -1,25 +1,42 @@
+import logging
+from pathlib import Path
 from PIL import Image
-import os
-from os.path import isfile, join
 import shutil
 
-source_folder = ""
-destination_folder = ""
-image_list = [f for f in os.listdir(source_folder) if isfile(join(source_folder, f))]
-counter = 0
+class AppleImageCopier:
+    def __init__(self, source_folder, destination_folder):
+        self.source_folder = Path(source_folder)
+        self.destination_folder = Path(destination_folder)
 
-def exif_apple(image):
-    try:
-        img = Image.open(image)
-        exif_data = img._getexif()
-        return exif_data.get(271)
-    except AttributeError as e:
-        pass
-    except IOError as e:
-        pass
+    def get_image_list(self):
+        """Returns a list of image files in the source folder."""
+        return [f.name for f in self.source_folder.iterdir() if f.is_file()]
 
-for i in image_list:
-    joined = join(source_folder, i)
-    if exif_apple(joined) == 'Apple':
-        print "Copying", i, "to", destination_folder
-        shutil.copy2(joined,destination_folder)
+    def exif_apple(self, image):
+        """Returns the value of the EXIF tag 271 (Make) for the given image file."""
+        try:
+            img = Image.open(image)
+            exif_data = img.getexif()
+            return exif_data.get(271)
+        except (AttributeError, IOError):
+            pass
+
+    def copy_apple_images(self):
+        """Copies all images taken with an Apple device from the source folder to the destination folder."""
+        # Check if source and destination folders exist
+        if not self.source_folder.exists():
+            logging.error(f"Source folder {self.source_folder} does not exist.")
+            return
+        if not self.destination_folder.exists():
+            logging.error(f"Destination folder {self.destination_folder} does not exist.")
+            return
+
+        # Get list of image files in source folder
+        image_list = self.get_image_list()
+
+        # Copy Apple images to destination folder
+        for image_filename in image_list:
+            image_path = self.source_folder / image_filename
+            if self.exif_apple(image_path) == 'Apple':
+                logging.info(f"Copying {image_filename} to {self.destination_folder}")
+                shutil.copy2(image_path, self.destination_folder)
